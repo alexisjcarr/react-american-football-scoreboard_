29d5f74f4d9a57c9ea07bca5fadbed916778e604
alexisjcarr <acarr59@gmail.com>
2019-09-09 15:01:43 -0500

    🔥 Adds conditionally rendered button that pauses/starts time and a reset button.
     #1

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,26 +1,55 @@
 import React, { useState, useEffect } from "react";
 
-const Timer = props => {
+const styles = {
+  display: "flex",
+  flexDirection: "column"
+};
+
+const Timer = () => {
   const [seconds, setSeconds] = useState(900); //15 minutes in seconds
+  const [running, setRunning] = useState(false);
+
+  const toggleRunning = () => {
+    setRunning(!running);
+  };
+
+  const reset = () => {
+    setSeconds(900);
+  };
 
   useEffect(() => {
-    let timer = setInterval(() => {
-      setSeconds(seconds - 1);
-    }, 1000);
+    let timer;
+    if (running) {
+      timer = setInterval(() => {
+        setSeconds(seconds - 1);
+      }, 1000);
+    } else if (!running && seconds !== 0) {
+      clearInterval(timer);
+    }
+
+    if (seconds === 0) {
+      clearInterval(timer);
+    }
 
     return () => clearInterval(timer);
-  }, [seconds]);
+  }, [running, seconds]);
 
   let minutes =
     Math.floor(seconds / 60) < 10
-      ? 0(Math.floor(seconds / 60))
+      ? `0${Math.floor(seconds / 60)}`
       : Math.floor(seconds / 60);
 
   let seconds_ = seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
 
   return (
-    <div className="timer">
-      {minutes}:{seconds_}
+    <div style={styles}>
+      <div className="timer">
+        {minutes}:{seconds_}
+      </div>
+      <button onClick={() => toggleRunning()}>
+        {running ? "PAUSE TIME" : "START TIME"}
+      </button>
+      <button onClick={() => reset()}>RESET</button>
     </div>
   );
 };
